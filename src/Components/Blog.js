9095186a229b78e265d429068511a53bcfd49ccc
RSibaja88 <54650378+RSibaja88@users.noreply.github.com>
2020-08-26 17:12:58 -0400

    fixed typos

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -8,7 +8,7 @@ export default function Blog() {
         <header className="titleB">From the Developer</header>
         <p>
           When I was first begining development on this web application, I
-          weighed which language I wanted to tackle Conway's Game of life.
+          weighed which language I wanted touse to tackle Conway's Game of life.
           Ultimately, I chose React because I'm more comfortable working with it
           than the other langauges I was considering and because I knew I could
           knock the UI out of the park with React.
@@ -24,15 +24,15 @@ export default function Blog() {
         <p>
           Before I even began coding, I knew exactly what I wanted to do with
           the UI and over all design. And while some of my design elements added
-          a few hurdles to jump over, (video loop, I'm looking at you) I really
-          feel those decsions were completely worth it for the end product. Go
-          big or go home, am i right? I made the video loop from a choppier
-          version and really smoothed it out and made it pretty seamless. I also
-          created all the graphics for this application using Inkscape and Gimp.
+          a few hurdles to jump over, I really feel those decsions were
+          completely worth it for the end product. Go big or go home, am I
+          right? I made the video loop from a choppier version and really
+          smoothed it out and made it pretty seamless. I also created all the
+          graphics for this application using Inkscape and Gimp.
         </p>
         <p>
-          If you would like to contact to discuss this project or any other
-          front-end project, you can email me at: rodrigosibaja88@outlook.com{" "}
+          If you would like to contact me to discuss this project or any other
+          front-end project, please email me at: rodrigosibaja88@outlook.com{" "}
         </p>
       </div>
     </section>
