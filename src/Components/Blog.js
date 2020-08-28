53ad2232539e72b3499d7b08c0fe9c427ccb05e2
RSibaja88 <54650378+RSibaja88@users.noreply.github.com>
2020-08-27 21:07:00 -0400

    .

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -8,10 +8,10 @@ export default function Blog() {
         <header className="titleB">From the Developer</header>
         <p>
           When I was first begining development on this web application, I
-          weighed which language I wanted touse to tackle Conway's Game of life.
-          Ultimately, I chose React because I'm more comfortable working with it
-          than the other langauges I was considering and because I knew I could
-          knock the UI out of the park with React.
+          weighed which language I wanted to use to tackle Conway's Game of
+          life. Ultimately, I chose React because I'm more comfortable working
+          with it than the other langauges I was considering and because I knew
+          I could knock the UI out of the park with React.
         </p>
         <p>
           First things first, I got to work on the Grid itself and added the
@@ -26,9 +26,8 @@ export default function Blog() {
           the UI and over all design. And while some of my design elements added
           a few hurdles to jump over, I really feel those decsions were
           completely worth it for the end product. Go big or go home, am I
-          right? I made the video loop from a choppier version and really
-          smoothed it out and made it pretty seamless. I also created all the
-          graphics for this application using Inkscape and Gimp.
+          right? I created all logo graphic for this application using Inkscape
+          and Gimp.
         </p>
         <p>
           If you would like to contact me to discuss this project or any other
